655e8352500cbc42b310e32d4c87dda7d95d24b5
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-10-30 14:43:23 +0100

    finished project. problem with the structure of app.jsx solved (line 9 and 22) is <> and </>

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,25 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route, Link } from 'react-router-dom';
 import Home from './pages/Home'
-import Proyects from './pages/Proyects'
+import Projects from './pages/Projects'
 import Resume from './pages/Resume'
 import Footer from './components/Footer'
 
 const App = () => {
     return (
-        <Router>
+        <>
                 <nav className="headerNav">
                     <Link to='/'>⌂ Home</Link> |
-                    <Link to='/proyects'>Proyects</Link> |
-                    <Link to='/resume'>Resume Cv</Link> |
+                    <Link to='/projects'> Projects</Link> |
+                    <Link to='/resume'> Resume Cv</Link> |
                 </nav>
             <Routes>
                 <Route path='/' element={<Home />} />
-                <Route path='/proyects' element={<Proyects />} />
+                <Route path='/projects' element={<Projects />} />
                 <Route path='/resume' element={<Resume />} />
             </Routes>
             
             <Footer />
-        </Router>
+        </>
     )
 }
 
