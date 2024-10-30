6b2b84aa2dd9d37f07ddbdeec44804d4f9d87bca
Verónica I Pérez S <v.isabel1989@gmail.com>
2024-10-30 14:14:12 +0100

    Resume in progress

---
+++ src/pages/Resume.jsx
@@ -0,0 +1,34 @@
+import { experiences, studies } from '../data/resume';
+
+function Resume() {
+  return (
+    <div className='bodyResume'>
+        <div className='jobs'>
+            <h1>Cv.</h1>
+                <h2>Experiencia Laboral</h2>
+                <ul>
+                    {experiences.map((experience) => (
+                        <li key={experience.id}>
+                            <h3>{experience.title} - {experience.company}</h3>
+                            <p>{experience.date}</p>
+                        </li>
+                    ))}
+                </ul>
+        </div>
+        <div className='study'>
+            <h2>Formacion Academica</h2>
+            <ul>
+                {studies.map((study) => (
+                    <li key={study.id}>
+                        <h3>{study.title}</h3>
+                        <p>{study.institution}</p>
+                        <p>{study.date}</p>
+                    </li>
+                ))}
+            </ul>       
+        </div>    
+    </div>
+  );
+}
+
+export default Resume;
