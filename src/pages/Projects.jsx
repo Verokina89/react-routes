1d65ac355647517ac05e16e76b5555046f3b4cfe
Verónica I Pérez S <v.isabel1989@gmail.com>
2025-02-25 00:09:55 +0100

    clean code

---
+++ src/pages/Projects.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import projects from '../data/projects';
+
+const Projects = () => {
+  return (
+    <div className='bodyProycts'>
+      <h1>Proyectos Realizados</h1>
+      <ul>
+        {projects.map((project) => (
+          <li key={project.id}>
+            <h2>{project.name}</h2>
+            <p>{project.description}</p>
+            <img src={project.image} alt={`${project.name}image`} />
+            <a href={project.url} target="_blank" rel="noopener noreferrer">Ver imagen del Proyecto</a>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Projects;
