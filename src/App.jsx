fa9a5376d2be8bdfba26f4d3bd8e863bde42c299
Veronica Isabel Perez Suarez <veronicaisabelperezsuarez@MacBook-Pro-de-Veronica-2017.local>
2025-02-27 23:40:27 +0100

    el fragment agrupa una lista de hijos sin agregar nodos extra al DOM. Simplemente envuelve y mantiene un clean code. Si se utiliza un section, no es necesario un fragment. Tecnicamente; un fragment se refiere a una característica de React que te permite agrupar un conjunto de elementos hijos sin agregar un nodo adicional al DOM (Documento Object Model) resultante

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,6 @@ export default App
 
 
 /*notas: el fragment agrupa una lista de hijos sin agregar nodos extra al DOM. Simplemente envuelve y mantiene un clean code. Si se utiliza un section, no es necesario un fragment. Tecnicamente; "un fragment se refiere a una característica de React que te permite agrupar un conjunto de elementos hijos sin agregar un nodo adicional al DOM (Documento Object Model) resultante."
-
 */
 
 /**
--- src/main.jsx
+++ src/main.jsx
@@ -11,8 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </React.StrictMode>
 )
 
-
-
-
 //notas: 
 // (referente a linea8):los componentes <App/> tienen que ir en mayuscula para que pueda reconocerlo; porque desntro trataba una clase y las clses tienen que ir en mayuscula. 
