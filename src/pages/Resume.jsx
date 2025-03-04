61ce2fae2f158881ba21a6816548597dbb0883a9
Veronica Isabel Perez Suarez <veronicaisabelperezsuarez@MacBook-Pro-de-Veronica-2017.local>
2025-03-04 21:12:47 +0100

    se añade img para agregarla al codigo en Resume.jsx y posteriormente mejorar las CSS

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,15 +5,16 @@ function Resume() {
     <div className='bodyResume'>
         <div className='jobs'>
             <h1>Verónica Isabel Pérez Suárez</h1>
-                <h2>Experiencia Laboral</h2>
-                <ul>
-                    {experiences.map((experience) => (
-                        <li key={experience.id}>
-                            <h3>{experience.title} - {experience.company}</h3>
-                            <p>{experience.date}</p>
-                        </li>
-                    ))}
-                </ul>
+            <img src="/src/img/foto1.jpeg" alt="fotoTipoCarnet" />
+            <h2>Experiencia Laboral</h2>
+            <ul>
+                {experiences.map((experience) => (
+                    <li key={experience.id}>
+                        <h3>{experience.title} - {experience.company}</h3>
+                        <p>{experience.date}</p>
+                    </li>
+                ))}
+            </ul>
         </div>
         <div className='study'>
             <h2>Formacion Academica</h2>
