a995ad4fadf1ae2a7a93817745e39aaac4a55e0e
Verónica I Pérez S <v.isabel1989@gmail.com>
2025-03-07 19:19:47 +0100

    cambios de estilos css y aspecto del navbar

--- src/components/HeaderNav.jsx
+++ src/components/HeaderNav.jsx
@@ -4,10 +4,10 @@ const HeaderNav = () => {
     
     return (
         <nav className="headerNav">
-            <NavLink to='/' className={({isActive}) => isActive ? "active" : ""} >🏠 Home</NavLink> |
-            <NavLink to='/projects' className={({isActive}) => isActive ? "active" : ""}> 🚀 Proyectos</NavLink> |
-            <NavLink to='/resume' className={({isActive}) => isActive ? "active" : ""}> 👩‍💻 Resumen Profesional</NavLink> |
-            <NavLink to='/contact' className={({isActive}) => isActive ? "active" : ""}> 📱 Contacto</NavLink>
+            <NavLink to='/' className={({isActive}) => isActive ? "active" : ""} > Home</NavLink> |
+            <NavLink to='/projects' className={({isActive}) => isActive ? "active" : ""}>  Proyectos</NavLink> |
+            <NavLink to='/resume' className={({isActive}) => isActive ? "active" : ""}> Resumen Profesional</NavLink> |
+            <NavLink to='/contact' className={({isActive}) => isActive ? "active" : ""}> Contacto</NavLink>
         </nav>
     );
 };
