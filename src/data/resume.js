26a43c48c7a741dfbec5f319926de3066819a8c7
Veronica Isabel Perez Suarez <v.isabel1989@gmail.com>
2025-03-11 20:05:03 +0100

    modificaciones en informacion de datos JS y Css para mejorar frontend

--- src/data/resume.js
+++ src/data/resume.js
@@ -24,19 +24,22 @@ const experiences = [
     id: 1,
     title: 'Sale Assistant Apple Products Specialist',
     company: 'K-tuin · Apple Premium Partner',
+    tasks:'Diagnóstico y soporte técnico de dispositivos Apple en tienda (iPhone, iPad, Mac, Apple Watch). Asesoramiento personalizado a clientes sobre productos y servicios Apple. Coordinación con el equipo técnico para garantizar soluciones rápidas y efectivas. Demostraciones de productos y formación básica a clientes',
     date: '2024 - Actualmente',
   },
   {
     id: 2,
     title: 'Technical Service in Mobile Repair',
     company: 'Save. Phone Service (El Corte Inglés)',
+    tasks:'Apertura y cierre de la tienda. Servicio Técnico y soporte (software). Asesorar al cliente, control de Stock, y gestión de mercancía, realización de pedidos y devoluciones, arqueo de caja (apertura y cierre).',
     date: 'Durante el 08/2023 hasta el 09/2021',
   },
   {
     id: 3,
-    title: 'Mobile and Tablet Repair Technician',
-    company: 'iRiparo España',
-    date: 'En el periodo del 08/2023 al 09/2021',
+    title:'Mobile and Tablet Repair Technician',
+    company: 'iRiparo España / Prink',
+    tasks:'Apertura y cierre de la tienda. Servicio Técnico y soporte (software) en dispositivos móviles e impresoras, diagnosticos, reparación. Asesorar al cliente, venta de moviles y accesorios, control de Stock, y gestión de mercancía, realización de pedidos y devoluciones, arqueo de caja (apertura y cierre).',
+    date:'En el periodo del 08/2023 al 09/2021',
   },
 ];
 
--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -1,36 +1,57 @@
 import { experiences, studies } from '../data/resume';
 import React from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../assets/img/logoDevprogrammer.png'
+import logo from '../assets/img/logoDevprogrammer.png';
+// import from '';
+// import from '';
+// import from '';
+// import from '';
+
 
 function Resume() {
   return (
     <div className='bodyResume'>
         <img src={logo} alt="logo" />
-        <h1>Conocimientos & Experiencias</h1>
-        <div className='studyCard'>
-            <h2>Me he Formado Academicamente como:</h2>
-            <ul>
-                {studies.map((study) => (
-                    <li key={study.id}>
-                        <h3>{study.title}</h3>
-                        <p>{study.institution}</p>
-                        <p>{study.date}</p>
-                    </li>
-                ))}
-            </ul>       
+        <h1>Conocimientos, Experiencias y Tecnologías</h1>
+        <div className='profProfile'>
+                <p>
+                Soy una profesional dedicada, con constancia e inclinada al aprendizaje continuo, siempre enfocada en el cumplimiento de objetivos. Me gusta trabajar en equipo y siempre me comprometo al 100% con mis responsabilidades laborales. Recientemente, he finalizado un Bootcamp de Full Stack Web Developer, lo que ha expandido mis habilidades en desarrollo y programación. Estoy en constante aprendizaje y práctica de los lenguajes y tecnologías adquiridas, como HTML, CSS, JavaScript, React, y frameworks como Node.js, Express, y CORS. Además, tengo experiencia en la creación y gestión de bases de datos, incluyendo SQL (MySQL), MongoDB y Testing con Jest.
+
+                En paralelo, cuento con sólida formación en tecnologías para dispositivos móviles, con experiencia en servicio y soporte técnico, incluyendo reparación de celulares y tablets. Soy licenciada en Periodismo Corporativo, con experiencia en marketing digital, diseño gráfico para webs, y gestión de redes sociales como Community Manager. Estoy capacitada para gestionar campañas de comunicación interna y externa.
+
+                Me considero una persona comunicativa, organizada y planificada, con habilidades de liderazgo y siempre comprometida con la ética empresarial. La redacción y la ortografía son puntos fuertes en mi perfil. Me apasiona trabajar por el cumplimiento de metas y objetivos, y me defino como creativa, innovadora, proactiva y dinámica. Además, tengo habilidades destacadas en la resolución de conflictos, lo que me permite manejar situaciones complejas de manera efectiva. 
+                </p>
         </div>
-        <div className='jobsCard'>
-            <h2>Laboralmente me he desempeñado como:</h2>
-            <ul>
-                {experiences.map((experience) => (
-                    <li key={experience.id}>
-                        <h3>{experience.title} - {experience.company}</h3>
-                        <p>{experience.date}</p>
-                    </li>
-                ))}
-            </ul>
-        </div>
+            <div className='studyCard'>
+                <h2>Estoy formada academicamente como:</h2>
+                <ul>
+                    {studies.map((study) => (
+                        <li key={study.id}>
+                            <h3>- {study.title}.</h3>
+                            <h4>{study.institution}.</h4>
+                            <h4>{study.date}.</h4>
+                        </li>
+                    ))}
+                </ul>       
+            </div>
+            <div className='jobsCard'>
+                <h2>Laboralmente me he desempeñado como:</h2>
+                <ul>
+                    {experiences.map((experience) => (
+                        <li key={experience.id}>
+                            <p>{experience.title} - {experience.company}</p>
+                            <h3>{experience.tasks} </h3>
+                            <h4>{experience.date}</h4>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            {/* <div className='logosLenguajesHerramientas'>
+                <img src={} alt="logo" />
+                <img src={} alt="logo" />
+                <img src={} alt="logo" />
+                <img src={} alt="logo" />
+            </div> */}
         <br />
         <Link to="/">Inicio</Link>    
     </div>
