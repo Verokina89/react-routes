36b8b6a04c1d366fecbe36cbd5a2a8cc82dc83c9
Veronica Isabel Perez Suarez <v.isabel1989@gmail.com>
2025-04-02 13:31:01 +0200

    detalles mejorados para las media query 300 y cambio de titulo en nombres del proyecto

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,21 +5,21 @@ import shopKianela from '../assets/img/shopbykianelaProjectReact.png';
 const projects = [
   {
     id: 1,
-    name: 'Vips Dasboard Project Break Frontend',
+    name: 'Vips Dasboard Project Break Front',
     image: dashboardProject,
     description: 'Creacion de un web basada en un Dashboard que contiene herramientas de ayudas para el escritorio personal.',
     url: 'https://verokina89.github.io/project-break-dashboard/index.html',
   },
   {
     id: 2,
-    name: 'BBDD Project Break Backend',
+    name: 'BBDD Project Break Back',
     image: projectBreak,
     description: 'Creación de una base de datos desde cero con MongoDB, deploy con Render.',
     url: 'https://projectbreak-back-react.onrender.com/api/products',
   },
   {
     id: 3,
-    name: 'Shop Kianela ProjectReact',
+    name: 'Shop Kianela Project React',
     image: shopKianela,
     description: 'Proyecto final que combina todos los lenguajes y tecnologias aprendidos en el bootcamp. Partiendo del uso de projectos anteriores; como la api de productos, creada y renderizada que se reutiliza en esta app (manejada con mongoDB). Se combinan proyectos reutilizables pero con las modificacioens pertinentes para el lenguaje (React)',
     url: 'https://luxury-gecko-33bf63.netlify.app',
